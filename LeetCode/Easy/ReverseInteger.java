303c47463a2e21efea7854f7921120e746910889
prde9306 <prde9306@gmail.com>
2021-06-27 14:57:00 +0900

    reversinteger

--- LeetCode/Easy/ReverseInteger.java
+++ LeetCode/Easy/ReverseInteger.java
@@ -30,5 +30,23 @@ package LeetCode.Easy;
  */
 public class ReverseInteger {
 
+//평범하게 1 의 자릿수부터 더해가면 된다.
+//한 가지 주의할 점은 res 값이 int 의 범위를 벗어날 수 있기 때문에 long 타입으로 선언 후에 나중에 바꿔야 함.
+
+    public int reverse(int x) {
+        long res = 0;
+
+        while (x != 0) {
+            res *= 10;
+            res += x % 10;
+            x /= 10;
+        }
+
+        if (-Integer.MAX_VALUE <= res && res <= Integer.MAX_VALUE) {
+            return (int) res;
+        } else {
+            return 0;
+        }
+    }
 
 }
