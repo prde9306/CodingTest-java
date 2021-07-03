958065606010f7302f35a9414645944ff6cb4c36
prde9306 <prde9306@gmail.com>
2021-07-03 16:10:47 +0900

    BOJ

--- BOJ/BOJ_1920.java
+++ BOJ/BOJ_1920.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BOJ_1920 {
     //BufferedReader와 Scanner차이 명확히 알기
@@ -48,10 +49,10 @@ public class BOJ_1920 {
         }
 
     }
-    //sol2
+    //sol1
     public static void main3(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+       // int n = Integer.parseInt(br.readLine());
 
         String[] a = br.readLine().split(" ");
         HashSet<String> hs = new HashSet<>(Arrays.asList(a));
@@ -59,11 +60,58 @@ public class BOJ_1920 {
         int m = Integer.parseInt(br.readLine());
         String[] arrM = br.readLine().split(" ");
         for (int i=0; i<m; i++) {
+
             if (hs.contains(arrM[i])) {
                 System.out.println(1);
             } else {
                 System.out.println(0);
             }
         }
+    }
+
+    //sol2
+    //bSearch많이 사용됨
+    public static boolean bSearch(int[] arr, int target) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while(min <= max) {
+            int mid = (min + max) / 2;
+
+            if(arr[mid] == target) {
+                return true;
+            }else if(arr[mid] > target) {
+                max = mid - 1;
+            }else if(arr[mid] < target) {
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main4(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            if(bSearch(arr, target)) {
+                sb.append("1\n");
+            }else {
+                sb.append("0\n");
+            }
+        }
+        System.out.println(sb.toString());
     }
 }
