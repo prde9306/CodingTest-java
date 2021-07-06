c4a27c55c5039098c12ecf3650d45977c92aa1e0
prde9306 <prde9306@gmail.com>
2021-07-06 22:22:32 +0900

    BOJ

--- BOJ/SearchSort/BOJ_11650.java
+++ BOJ/SearchSort/BOJ_11650.java
@@ -10,33 +10,32 @@ import java.util.StringTokenizer;
 
 public class BOJ_11650 {
 
-    public static void main(String[] args)throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    int[][] arr = new int[N][2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
 
-    StringTokenizer st;
-    for(int i=0; i<N; i++) {
-        st = new StringTokenizer(br.readLine());
-        arr[i][0] = Integer.parseInt(st.nextToken());
-        arr[i][0] = Integer.parseInt(st.nextToken());
-    }
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+        }
 
-    Arrays.sort(arr, (e1,e2) -> {
+        Arrays.sort(arr, (e1, e2) -> {
 
-           if(e1[0] == e2[0]){
-               return e1[1] - e2[1];
-           }
-           else {
-               return e1[0] - e2[0];
-           }
-    });
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
 
-    StringBuilder sb = new StringBuilder();
-    for(int i =0; i<N; i++){
-        sb.append(arr[i][0] + " "+ arr[i][1]).append('\n');
-    }
-    System.out.println(sb);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
     }
 
     public static void main2(String[] args) {
@@ -47,21 +46,22 @@ public class BOJ_11650 {
 
         int[][] arr = new int[N][2];
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i][0] = in.nextInt();
             arr[i][1] = in.nextInt();
         }
 
         Arrays.sort(arr, (e1, e2) -> {
-            if(e1[0] == e2[0]) {
+            if (e1[0] == e2[0]) {
                 return e1[1] - e2[1];
             } else {
                 return e1[0] - e2[0];
             }
         });
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             System.out.println(arr[i][0] + " " + arr[i][1]);
         }
     }
+
 }
