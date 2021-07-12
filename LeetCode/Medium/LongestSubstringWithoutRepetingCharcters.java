31e9c54285e57c8135173ecd371cf7255cb526fd
prde9306 <prde9306@gmail.com>
2021-07-12 11:12:16 +0900

    Longest substring without repeating charcters

--- LeetCode/Medium/LongestSubstringWithoutRepetingCharcters.java
+++ LeetCode/Medium/LongestSubstringWithoutRepetingCharcters.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 //sol1 새로운 함수를 만들어야
-//근데 시간초과인데?
+//이건 시간초과
 public class LongestSubstringWithoutRepetingCharcters {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
@@ -26,5 +26,29 @@ public class LongestSubstringWithoutRepetingCharcters {
         }
         return true;
     }
+    //sol2
+
+        public int lengthOfLongestSubstring2(String s) {
+            // 한 글자씩만 나와야한다. ex, abca 는 a 가 두번나오므로 안된다.
+            int maxLength = 0;
+            int i=0, j=0;
+            int length = s.length();
+            HashSet<Character> stringSet = new HashSet<>();
+
+            while (j < length) {
+                if(i > j) break;
+                if (!stringSet.contains(s.charAt(j))){
+                    stringSet.add(s.charAt(j++));
+                    maxLength = Math.max(maxLength, j - i);
+                    continue;
+                }
+                stringSet.remove(s.charAt(i++));
+
+            }
+
+            return maxLength;
+        }
+
+
 }
 
