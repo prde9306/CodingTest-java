01cac427216da56475e88df14f7b500cbb3f2491
prde9306 <prde9306@gmail.com>
2021-07-14 16:10:26 +0900

    G-1

---
+++ GroomEdu/Pro1_G.java
@@ -0,0 +1,69 @@
+package GroomEdu;
+
+import java.util.Scanner;
+
+public class Pro1_G {
+
+    public static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * 배열에서 소속이 "AJOU"인 첫 원소와 마지막 원소를 출력하는 함수
+     * @param school 각 사람들의 소속학교 정보 배열
+     * @param n      사람들의 수
+     */
+    public static void printIndexes(String[] schools, int n)
+    {
+        int first = -1; //존재하지 않으면 -1
+        int last = -1;  //존재하지 않으면 -1
+        for(int k=n-1; k>=0; k--){
+            if(schools[k].equals("AJOU")){
+                first=k+1;
+            }
+        }
+        for(int j=0; j<n; j++){
+            if(schools[j].equals("AJOU")){
+                last=j+1;
+            }
+        }
+        System.out.printf("%d %d\n", first, last );
+    }
+
+    public static void main(String[] args)
+    {
+        int n = scanner.nextInt();
+        String[] schools = new String[n];
+        for(int i = 0 ; i < n ; i++)
+        {
+            schools[i] = scanner.next();
+        }
+
+        printIndexes(schools, n);
+    }
+
+    public static void printIndexes2(String[] school, int n)
+    {
+        int first = -1; //존재하지 않으면 -1
+        int last = -1;  //존재하지 않으면 -1
+
+        for(int i = 0 ; i < n ; i++)
+        {   // 모든 소속 school[i]에 대해 차례로
+
+            if( school[i].equals("AJOU") )
+            {   // 소속이 "AJOU"인 사람의 번호 i에 대해
+
+                if(first == -1)
+                {   // [0, i-1]에서 소속이 "AJOU"인 사람이 등장한 적 없던 경우
+                    // 이번이 첫 등장이다
+                    first = i + 1;
+                }
+
+                // 소속이 "AJOU"인 사람이 등장한 경우 덮어쓴다
+                // 이번이 마지막 등장이다
+                last = i + 1;
+            }
+        }
+
+        System.out.printf("%d %d\n", first, last );
+    }
+
+}
