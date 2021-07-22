7784c97bb92c185c2069b54367ea7bd611790e21
prde9306 <prde9306@gmail.com>
2021-07-22 12:04:27 +0900

    BFS/DFS

---
+++ inflearn/BFS.java
@@ -0,0 +1,49 @@
+package inflearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Node2 {
+    int data;
+    Node2 lt, rt;
+
+    public Node2(int val) {
+        data = val;//1,2,3,4,5,6
+        lt = rt = null; // 노드라는 클래스의 객체주소를 저장
+    }
+
+    public class Main {
+        Node2 root;
+        public void BFS(Node root){
+            Queue<Node2> Q = new LinkedList<>();
+            Q.offer(root);
+            int L =0;
+            while (!Q.isEmpty()){
+                int len =Q.size();
+                System.out.print(L+" ");
+                for(int i=0; i<len; i++){
+                    Node cur = Q.poll();
+                    System.out.print(cur.data+ " ");
+                    if(cur.lt != null) Q.offer(cur.lt);
+                    if(cur.rt!=null) Q.offer(cur.rt);
+                }
+                L++;
+                System.out.println();
+            }
+
+    }
+
+        public static void main(String[] args) {
+            Main tree = new Main();
+            tree.root = new Node2(1);
+            tree.root.lt = new Node2(2);
+            tree.root.rt = new Node2(3);
+            tree.root.lt.lt = new Node2(4);
+            tree.root.lt.rt = new Node2(5);
+            tree.root.rt.lt = new Node2(6);
+            tree.root.rt.rt = new Node2(7);
+            tree.DFS(tree.root);
+
+        }
+    }
+}
---
+++ inflearn/Main.java
@@ -0,0 +1,39 @@
+package inflearn;
+//부분집합
+public class Main {
+    static int n;
+    //이 숫자를 부분집합으로 사용하는지 안하는지 check
+
+    static int[] ch;
+//L이라는 숫자를 원소로 사용하는지 안하는지 뻗어나간다.
+    public void DFS(int L){
+        //if-else로, 종착점에 왓을 때
+        if(L==n+1){
+            String tmp = "";
+            for(int i=1; i<=n; i++){
+                if(ch[i]==1) tmp+=(i+" ");
+            }
+            if(tmp.length()>0)System.out.println(tmp);
+
+        }else {
+            //왼쪽으로 뻗고, 뻗기 전에 사용여부
+            ch[L]=1;
+            DFS(L+1);
+            //오른쪽으로 뻗고
+            ch[L]=0;
+            DFS(L+1);
+        }
+
+
+
+    }
+    public static void main(String[]args){
+        Main T = new Main();
+        n =3;
+        //check배열을 원소를 인덱스번호로
+        ch = new int[n+1];
+        T.DFS(1);
+
+    }
+
+}
