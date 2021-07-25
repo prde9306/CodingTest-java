b356d93f3063d2318e985ef9fd43637aac83a3cf
prde9306 <prde9306@gmail.com>
2021-07-25 16:53:20 +0900

    DFS

---
+++ inflearn/DFSBFSLv2/baduk.java
@@ -0,0 +1,29 @@
+package inflearn.DFSBFSLv2;
+
+public class baduk {
+    /**class Main{
+     static int answer = Integer.Min_VALUE, c, n;
+     public void DFS(int L, int sum, int[]arr){
+     if(sum>c) return;
+     if(L==n){
+     answer = Math.max(answer, sum);
+     }
+     }else{
+     DFS(L+1, sum+arr[L], arr);
+     DFS(L+1, sum, arr);
+     }
+     }
+     public static void main(String[]args){
+     Main T = new Main();
+     Scanner sc = new Scanner(System.in);
+     c = sc.nextInt();
+     n = sc.nextInt();
+     int[] arr = new int[n];
+     for(int i=0; i<n; i++){
+     arr[i]=sc.nextInt();
+     }
+     T.DFS(0,0,arr);
+     System.out.println(answer);
+     }
+     **/
+}
