409a68cb4def39d81c5455f23d09ec61985e586e
prde9306 <prde9306@gmail.com>
2021-08-12 16:06:46 +0900

    210812 codingTest exercise

--- inflearn/chap01/pro2.java
+++ inflearn/chap01/pro2.java
@@ -1,42 +1,45 @@
 package inflearn.chap01;
 
+import java.util.Scanner;
+
 public class pro2 {
     /**
-     * class Main{
-     *
-     *     public String solution(String []in){
-     *         String answer="";
-     *
-     *         for(char x : str.ToCharArray()){
-     *             if(Character.isLowerCase(x)) answer+= Character.toUpperCase(x);
-     *             else answer+= Character.toLowerCase(x);
-     *         }
-     *         return answer;
-     *
-     *         //ASCII로 해도 된다.
-     *         //대문자 65~90
-     *         //소문자 97~122
-     *         //(32 차이남)
-     *         for(char x : str.ToCharArray()){
-     *             //대문자
-     *             if(x>=65&& x<=90) answer+=(char)(x-32);
-     *             else answer+=(char)(x+32);
-     *         }
-     *
-     *         return answer;
-     *
-     *
-     *     }
-     *
-     *
-     *
-     *     public static void main(String[]args){
-     *         Main T = new Main();
-     *
-     *         Scanner sc = new Scanner(System.in);
-     *         String str= sc.next();
-     *         System.out.println(T.solution(str));
-     *     }
-     * }
+     * Uppercase, LowerCase어떻게 바꾸는지 잘봐 IDE도움없이 Primitive, Wrapper
      */
+
+      class Main{
+
+          public String solution(String str){
+
+              String answer="";
+
+              char[] ch = str.toCharArray();
+
+              for(char x : str.toCharArray()){
+                  if(Character.isLowerCase(x)) answer+= Character.toUpperCase(x);
+                  else answer+= Character.toLowerCase(x);
+              }
+              return answer;
+
+              //ASCII로 해도 된다.
+              //대문자 65~90
+              //소문자 97~122
+              //(32 차이남)
+              for(char x : str.toCharArray()){
+                  //대문자로
+                  if(x>=97&& x<=122) answer+=(char)(x-32);
+                  else answer+=(char)(x+32);
+              }
+              return answer;
+          }
+
+          public static void main(String[]args){
+              Main T = new Main();
+
+              Scanner sc = new Scanner(System.in);
+              String str= sc.next();
+              System.out.println(T.solution(str));
+          }
+      }
+
 }
---
+++ inflearn/chap07/pro2.java
@@ -0,0 +1,56 @@
+package inflearn.chap07;
+
+public class pro2 {
+    /**
+     * // 회의가 가장 빨리끝난 걸 기준으로 계속 선택해야함&& 시작시간 기준으로 오름차순 . 그래야 최대한 많이 선택할 수 있음
+     * // 그리디 : 가장 닥친 것부터 해결
+     * class Room implements Comparable<Room>{
+     *     public int s,e;
+     *     Body(int s, int e){
+     *         this.s = s;
+     *         this.e = e;
+     *     }
+     *     @Override
+     *     public int compareTo(Room o){
+     *         if(this.e==o.e) return this.s-o.s;
+     *         else return this.e-o.e;
+     *         //오름차순 정렬
+     *     }
+     * }
+     *
+     * class Main{
+     *
+     *     public int solution(int N, ArrayList<Room>arr){
+     *         int cnt =0;
+     *         Collection.sort(arr);
+     *         //위에있는 정렬기준에 의해 정렬됨
+     *         //현재 하고 있는 회의의 끝나는 시간
+     *         int et = 0;
+     *         for(Time bo : arr){
+     *             if(ob.s>=et){
+     *                 cnt++;
+     *                 et=ob.e;
+     *             }
+     *         }
+     *         return cnt;
+     *         }
+     *
+     *     public static void main(String[] args)throws IOException {
+     *
+     *         Main T = new Main();
+     *         Scanner sc = new Scanner(System.in);
+     *
+     *         int N = sc.nextInt();
+     *         ArrayList<Room> arr = new ArrayList<>();
+     *
+     *         for(int i=0; i<N; i++){
+     *             int s =sc.nextInt();
+     *             int e =sc.nextInt();
+     *
+     *             arr.add(new Body(s, e));
+     *         }
+     *
+     *     }
+     * }
+     */
+}
