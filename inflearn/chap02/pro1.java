b84a9498ab19ddfa232c9f8963717397a8558c76
prde9306 <prde9306@gmail.com>
2021-08-15 16:40:36 +0900

    210815 CodingTest review

--- inflearn/chap02/pro1.java
+++ inflearn/chap02/pro1.java
@@ -1,32 +1,37 @@
-package inflearn.chap02;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+/**
+ * ArrayList 똑바로 활용할 수 있는가.
+ */
+class Main{
 
-public class pro1 {
+    public ArrayList<Integer> solution(int N, int[]arr){
+        ArrayList<Integer> answer = new ArrayList<>();
 
-    class Main{
-
-        public ArrayList<Integer> solution(int n, int[]arr){
-            ArrayList<Integer> answer = new ArrayList<>();
-            answer.add(arr[0]);
-            for(int i=1; i<N; i++){
-                if(arr[i]> arr[i-1]) answer.add(arr[i]);
-
-                return answer;
-
+        answer.add(arr[0]);
+        for(int i=0; i<N-1; i++){
+            if(arr[i]<arr[i+1]){
+                answer.add(arr[i+1]);
             }
         }
-        public static void main(String[]args)throws IOException{
-            Main T = new Main();
-            Scanner sc = new Scanner(System.in);
-            int N = sc.nextInt();
-            int []arr  = new int[N];
+        return answer;
+    }
 
-            for(int i=0; i<N; i++){
-                arr[i] = sc.nextInt();
-            }
-            for(int x : T.solution(n, arr)){
-                System.out.print(x+" ");
-            }
+    public static void main (String[] args)throws IOException{
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int []arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i]= sc.nextInt();
         }
+
+        for(int x: T.solution(N, arr)){
+            System.out.print(x+" ");
+        }
+
+
     }
 }
--- inflearn/chap02/pro2.java
+++ inflearn/chap02/pro2.java
@@ -1,35 +1,49 @@
-package inflearn.chap02;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class pro2 {
-    /**
-     * import java.io.IOException;
-     * import java.util.Scanner;
-     *
-     * class Main{
-     *
-     *     //이중 포문으로 풀면 시간제한결러
-     *     public int solution(int N, int[]arr){
-     *         int answer =1, max =arr[0];
-     *         for(int i=1; i<N; i++){
-     *            if(arr[i]> max) answer++;
-     *            max = arr[i];
-     *         }
-     *         return answer;
-     *     }
-     *
-     *
-     *     public static void main(String[]args)throws IOException{
-     *         Main T = new Main();
-     *         Scanner sc = new Scanner(System.in);
-     *
-     *         int N = sc.nextInt();
-     *         int[]arr = new int[N];
-     *
-     *         for(int i=0; i<N; i++){
-     *             arr[i]=sc.nextInt();
-     *         }
-     *         System.out.print(T.solution(N, arr));
-     *     }
-     * }
-     */
+class Main{
+
+    //이중 포문으로 풀면 시간제한결러
+    //sol1 이렇게 풀어도 되고
+
+    public int solution(int N, int[]arr){
+        int answer =1, max =arr[0];
+        for(int i=1; i<N; i++){
+            if(arr[i]> max) answer++;
+            max = arr[i];
+        }
+        return answer;
+    }
+
+    //sol2 이렇게 풀어도 된다. 메커니즘은 동일함.
+    public int solution2(int N, int[]arr){
+
+        int answer =1;
+
+        //최고 큰 키를 가진 사람 어디에 저장해놔야 할 듯?
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<N-1; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+            if(max<arr[i+1]){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[]args)throws IOException{
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[]arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(T.solution(N, arr));
+    }
 }
