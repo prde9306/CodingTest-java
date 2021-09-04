d4efed705f0b4cf1fe4ea593cca90c08eb94fb2a
prde9306 <prde9306@gmail.com>
2021-09-04 13:40:50 +0900

    210904 codingtest review

--- InflearnLecture/chap02Array/pro5.java
+++ InflearnLecture/chap02Array/pro5.java
@@ -27,3 +27,41 @@ class Main{
         T.solution(N);
     }
 }
+
+//밑에는 왜 안되나??? 내방식...
+
+class Main{
+
+    public boolean isPrime(int M){
+        boolean answer = false;
+        if(M==2||M==3){
+            answer=true;
+        }
+        else{
+            if(M%2==0 ||M%3==0) return false;
+            else{
+                answer =true;
+            }
+        }
+        return answer;
+    }
+
+    public int solution(int N){
+        int answer =0;
+        for(int i=2; i<=N; i++){
+            if(isPrime(i)) answer++;
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args)throws IOException {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int N =sc.nextInt();
+
+        System.out.print(T.solution(N));
+
+    }
+}
